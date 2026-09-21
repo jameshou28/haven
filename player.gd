82c17b3b6a0350c3796eba45943aff54258b4153
jameshou28 <james.william.hou@gmail.com>
2026-09-21 17:18:39 -0400

    feat: end conditions, tilemap update

--- player.gd
+++ player.gd
@@ -5,14 +5,18 @@ const SPEED = 400.0
 const JUMP_VELOCITY = -600.0
 const MAX_LIGHT = 3.0
 const DRAIN_RATE = 0.05
+const MAX_FALL_SPEED = 700.0
+
 
 @onready var light: PointLight2D = $PointLight2D
-var light_amount := MAX_LIGHT
+@onready var win_label: Label = $"../CanvasLayer/WinLabel"
+@onready var tiles: TileMapLayer = $"../TileMapLayer"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		velocity.y = min(velocity.y, MAX_FALL_SPEED)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -34,6 +38,12 @@ func _physics_process(delta: float) -> void:
 	
 	if light.texture_scale <= 0.0 or position.y > 2000:
 		get_tree().reload_current_scene()
+		
+	var cell := tiles.local_to_map(tiles.to_local(global_position))
+	print(cell)
+	if cell.y >= 19:
+		win_label.visible = true
+		set_physics_process(false)
 
 #func add_light(amount: float) -> void:
 	#light_amount = min(light_amount + amount, MAX_LIGHT)
