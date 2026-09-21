de11c0920bca1e936bc35e92fd66fc3aeb81bcc7
jameshou28 <james.william.hou@gmail.com>
2026-09-21 09:04:43 -0400

    feat: implement light drain

--- player.gd
+++ player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -600.0
+const MAX_LIGHT = 3.0
+const DRAIN_RATE = 0.05
 
+@onready var light: PointLight2D = $PointLight2D
+var light_amount := MAX_LIGHT
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +27,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	light_amount -= DRAIN_RATE * delta
+	light.texture_scale = max(light_amount, 0.0)
+	
+	if light_amount <= 0.0:
+		get_tree().reload_current_scene()
+
+func add_light(amount: float) -> void:
+	light_amount = min(light_amount + amount, MAX_LIGHT)
+	
